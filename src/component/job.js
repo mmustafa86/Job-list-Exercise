9b4e73db26727fa75491adcb1a82d45fb88e10d3
mohammed mustafa <mohammed.alkhddar@gmail.com>
2020-02-28 16:30:04 -0600

    3 left

---
+++ src/component/job.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class job extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+    <h1></h1>
+    );
+  }
+}
+
+export default job;
---
+++ src/component/jobs.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import JobsListItem from './JobsListItem'
+// import '../App.css'
+import job from './job'
+import '../component/Jobs.css'
+import axios from "axios"
+import {Switch, Route} from 'react-router-dom'
+export default class jobs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: []
+    };
+
+  }
+ 
+  componentDidMount() {
+    console.log('Did component mount ?');
+    axios.get('/api/jobs')
+      .then(({data}) => {
+        // console.log(data)
+        this.setState({jobs: data})
+      })
+     
+  }
+
+  render() {
+    console.log(this.state.jobs)
+    const  jobsJSX = this.state.jobs.map((job,index)=>{
+
+      return  <JobsListItem key={index} {...job}/>
+    })
+
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Jobs In Atlanta</h1>
+            <p className="App-subtitle">Click to explore  jobs</p>
+          </header>
+          <div className="Jobs">
+        {jobsJSX}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
